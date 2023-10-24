4f5623dbed5f7e880e2098623fb01c052f156be4
kaidzn <kaidzn17@gmail.com>
2023-10-25 00:55:17 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      'pageyellow': '#FFE51C'
+    },
+    fontFamily: {
+      poppins: ['Poppins', 'sans'],
+    }
+  },
+  plugins: [],
+}
+
