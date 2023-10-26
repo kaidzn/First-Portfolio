6701b6e9df512146083fac46f793fe2dde82fa0e
kaidzn <kaidzn17@gmail.com>
2023-10-27 00:23:54 +0600

    git commit -m "Portfolio"

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,16 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {},
+    screens: {
+      'sm' : '640px',
+      'md' : '768px',
+      'lg' : '1024px',
+      'xl' : '1280px',
+      '2xl': '1538px',
+    },
     colors: {
-      'pageyellow': '#FFE51C'
+      'pageyellow': '#FFE51C',
+      'pageblue': '#060725',
     },
     fontFamily: {
       poppins: ['Poppins', 'sans'],
